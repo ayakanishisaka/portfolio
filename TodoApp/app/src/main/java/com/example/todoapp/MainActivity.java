8c4dce871bf60feaca0873c3f6c6c55434bc9d3f
ayaka nishisaka <243kayaka@gmai.com>
2025-03-09 01:58:27 +0900

    TOPとList画面のコード入力

--- TodoApp/app/src/main/java/com/example/todoapp/MainActivity.java
+++ TodoApp/app/src/main/java/com/example/todoapp/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.todoapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,12 +11,30 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button btnNew = findViewById(R.id.btnNew);
+        Button btnList = findViewById(R.id.btnList);
+        btnNew.setOnClickListener(this);
+        btnList.setOnClickListener(this);
+    }
 
+    @Override
+    public void onClick(View v) {
+        if (v.getId() == R.id.btnNew) {
+            Intent intentNew = new Intent(this, New.class);
+            startActivity(intentNew);
+        }
+        if (v.getId() == R.id.btnList) {
+            Intent intentList = new Intent(this,List.class);
+            startActivity(intentList);
+        }
+        if(v.getId()==R.id.btnToTop){
+            finish();
+        }
     }
 }
--- TodoApp/app/src/main/java/com/example/todoapp/New.java
+++ TodoApp/app/src/main/java/com/example/todoapp/New.java
@@ -1,6 +1,7 @@
 package com.example.todoapp;
 
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +9,16 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class New extends AppCompatActivity {
+public class New extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_new);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
     }
+    public void onClick(View view){
+        finish();
+    }
+
 }
