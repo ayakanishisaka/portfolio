11a1cdebed2c596c77d45c2a7fdf7af60e92c8a8
ayakanishisaka <243kayaka@gmail.com>
2025-04-25 16:49:16 +0900

    editorメソッド追加

--- app/src/main/java/com/example/test/NewActivity.java
+++ app/src/main/java/com/example/test/NewActivity.java
@@ -1,5 +1,4 @@
 package com.example.test;
-
 import android.content.ContentValues;
 import android.content.Intent;
 import android.database.Cursor;
@@ -8,7 +7,6 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.CalendarView;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.Toast;
@@ -58,28 +56,9 @@ public class NewActivity extends AppCompatActivity implements View.OnClickListen
         //一覧画面のリストから選択した1件を再編集または新規登録かを判断する
         todoId = getIntent().getIntExtra("id", -1);// ID(int)情報を取得(もしキーがなければ-1)
 
-        if (todoId != -1) { // IDがあるなら再編集
-            db = dbHelper.getReadableDatabase();
-            Cursor cursor = db.query("todos", null, "id=?",
-                    new String[]{String.valueOf(todoId)}, null, null, null);
-            if (cursor.moveToFirst()) {
-                String editTitle = cursor.getString(cursor.getColumnIndexOrThrow("title"));
-                String editMemo = cursor.getString(cursor.getColumnIndexOrThrow("memo"));
-
-                title.setText(editTitle);
-                memo.setText(editMemo);
-
-                String dateString = cursor.getString(cursor.getColumnIndexOrThrow("date"));
-                String[] parts = dateString.split("/");
-                yearSpinner.setSelection(years.indexOf(parts[0]));
-                monthSpinner.setSelection(months.indexOf(parts[1]));
-                daySpinner.setSelection(days.indexOf(parts[2]));
-            }
-            cursor.close();
-            db.close();
-
-        } else {
-            // MainActivityのCalendarViewから渡された日付があるか確認
+        if (todoId != -1) { // IDが１以上なら登録済みの編集
+            editor(todoId);
+        } else { // TOPのカレンダー日付をクリックしていたか
             Intent intentDate = getIntent();
             String passedYear = intentDate.getStringExtra("year");
             String passedMonth = intentDate.getStringExtra("month");
@@ -105,104 +84,128 @@ public class NewActivity extends AppCompatActivity implements View.OnClickListen
         }
     }
 
-    private void spinners() {
-        years = new ArrayList<>();
-        months = new ArrayList<>();
-        days = new ArrayList<>();
+// IDがあるなら再編集
+    private void editor(int todoId){
+            db = dbHelper.getReadableDatabase();
 
-        for (int i = 2025; i <= 2035; i++) {
-            years.add(String.valueOf(i));
+            Cursor cursor = db.query("todos", null, "id=?",
+                    new String[]{String.valueOf(todoId)}, null, null, null);
+            if (cursor.moveToFirst()) {
+                String editTitle = cursor.getString(cursor.getColumnIndexOrThrow("title"));
+                String editMemo = cursor.getString(cursor.getColumnIndexOrThrow("memo"));
+
+                title.setText(editTitle);
+                memo.setText(editMemo);
+
+                String dateString = cursor.getString(cursor.getColumnIndexOrThrow("date"));
+                String[] parts = dateString.split("/");
+                yearSpinner.setSelection(years.indexOf(parts[0]));
+                monthSpinner.setSelection(months.indexOf(parts[1]));
+                daySpinner.setSelection(days.indexOf(parts[2]));
+            }
+            cursor.close();
+            db.close();
         }
-        for (int i = 1; i <= 12; i++) {
-            months.add(String.format("%02d", i));
-        }
-        for (int i = 1; i <= 31; i++) {
-            days.add(String.format("%02d", i));
-        }
 
-        //年・月・日の各スピナーにセットする
-        spinnerAdapter(yearSpinner, years);
-        spinnerAdapter(monthSpinner, months);
-        spinnerAdapter(daySpinner, days);
-    }
+        private void spinners () {
+            years = new ArrayList<>();
+            months = new ArrayList<>();
+            days = new ArrayList<>();
 
-    // アダプター（配列をスピナーに接続するもの）を使ってスピナーに選択肢を設定
-    private void spinnerAdapter(Spinner spinner, List<String> data) {
-        // 現在の Context（このActivity）にアダプターを使ってdataをString型で一行表示する
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
-
-        // アダプターにドロップダウンを設定
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-
-        // 受け取ったスピナーに上記レイアウトのアダプターをセットして表示
-        spinner.setAdapter(adapter);
-    }
-
-    @Override
-    public void onClick(View v) {
-        int id = v.getId();
-
-        if (id == R.id.btnList) {
-
-            String titleS = title.getText().toString();
-            String memoS = memo.getText().toString();
-            String year = yearSpinner.getSelectedItem().toString();
-            String month = monthSpinner.getSelectedItem().toString();
-            String day = daySpinner.getSelectedItem().toString();
-            String dateS = year + "/" + month + "/" + day;
-
-            // 正しい日付かチェック
-            if(!dateCheck(dateS)){
-                Toast.makeText(this, "存在しない日付です", Toast.LENGTH_SHORT).show();
-                return;
+            for (int i = 2025; i <= 2035; i++) {
+                years.add(String.valueOf(i));
+            }
+            for (int i = 1; i <= 12; i++) {
+                months.add(String.format("%02d", i));
+            }
+            for (int i = 1; i <= 31; i++) {
+                days.add(String.format("%02d", i));
             }
 
-            if (titleS.equals("")) {
-                Toast.makeText(this, "タイトルが未入力です", Toast.LENGTH_SHORT).show();
-            } else {
-                SQLiteDatabase db = dbHelper.getWritableDatabase(); // 書き込み用のDBを開く
+            //年・月・日の各スピナーにセットする
+            spinnerAdapter(yearSpinner, years);
+            spinnerAdapter(monthSpinner, months);
+            spinnerAdapter(daySpinner, days);
+        }
 
-                values = new ContentValues(); // 保存する内容をまとめる
-                values.put("title", titleS);
-                values.put("memo", memoS);
-                values.put("date", dateS);
+// アダプター（配列をスピナーに接続するもの）を使ってスピナーに選択肢を設定
+        private void spinnerAdapter (Spinner spinner, List < String > data){
+            // 現在の Context（このActivity）にアダプターを使ってdataをString型で一行表示する
+            ArrayAdapter<String> adapter
+                    = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, data);
 
-                if (todoId != -1) { // IDがあれば更新
-                    int upId = db.update("todos", values, "id = ?", new String[]{String.valueOf(todoId)});
-                    if (upId > 0) {
-                        Toast.makeText(this, "更新されました", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(this, "更新に失敗しました", Toast.LENGTH_SHORT).show();
-                    }
-                } else { // 更新するIDがなかったら(-1のまま）新規登録
-                    long newId = db.insert("todos", null, values); //新規登録
-                    if (newId != -1) {
-                        Toast.makeText(this, "登録されました", Toast.LENGTH_SHORT).show();
-                    } else {
-                        Toast.makeText(this, "登録に失敗しました", Toast.LENGTH_SHORT).show();
-                    }
+            // アダプターにドロップダウンを設定
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+            // 受け取ったスピナーに上記レイアウトのアダプターをセットして表示
+            spinner.setAdapter(adapter);
+        }
+
+        @Override
+        public void onClick (View v){
+            int id = v.getId();
+
+            if (id == R.id.btnList) {
+
+                String titleS = title.getText().toString();
+                String memoS = memo.getText().toString();
+                String year = yearSpinner.getSelectedItem().toString();
+                String month = monthSpinner.getSelectedItem().toString();
+                String day = daySpinner.getSelectedItem().toString();
+                String dateS = year + "/" + month + "/" + day;
+
+                // 正しい日付かチェック
+                if (!dateCheck(dateS)) {
+                    Toast.makeText(this, "存在しない日付です", Toast.LENGTH_SHORT).show();
+                    return;
                 }
-                // 一覧に戻る
-                Intent intentList = new Intent(this, ToDoListActivity.class);
-                startActivity(intentList);
+
+                if (titleS.equals("")) {
+                    Toast.makeText(this, "タイトルが未入力です", Toast.LENGTH_SHORT).show();
+                } else {
+                    SQLiteDatabase db = dbHelper.getWritableDatabase(); // 書き込み用のDBを開く
+
+                    values = new ContentValues(); // 保存する内容をまとめる
+                    values.put("title", titleS);
+                    values.put("memo", memoS);
+                    values.put("date", dateS);
+
+                    if (todoId != -1) { // IDがあれば更新
+                        int upId = db.update("todos", values, "id = ?", new String[]{String.valueOf(todoId)});
+                        if (upId > 0) {
+                            Toast.makeText(this, "更新されました", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "更新に失敗しました", Toast.LENGTH_SHORT).show();
+                        }
+                    } else { // 更新するIDがなかったら(-1のまま）新規登録
+                        long newId = db.insert("todos", null, values); //新規登録
+                        if (newId != -1) {
+                            Toast.makeText(this, "登録されました", Toast.LENGTH_SHORT).show();
+                        } else {
+                            Toast.makeText(this, "登録に失敗しました", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                    // 一覧に戻る
+                    Intent intentList = new Intent(this, ToDoListActivity.class);
+                    startActivity(intentList);
+                    finish();
+                }
+            }
+            if (id == R.id.btnToTop) {
+                Intent intentToTop = new Intent(this, MainActivity.class);
+                startActivity(intentToTop);
                 finish();
             }
         }
-        if (id == R.id.btnToTop) {
-            Intent intentToTop = new Intent(this, MainActivity.class);
-            startActivity(intentToTop);
-            finish();
-        }
-    }
 
-    private boolean dateCheck(String dateS) {
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
-        sdf.setLenient(false); // 実在するかチェック falseでエラー出す true(デフォ)は勝手に補正される
-        try {
-            Date date = sdf.parse(dateS);
-            return true;
-        }catch (ParseException e){
-            return false;
+        private boolean dateCheck (String dateS){
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+            sdf.setLenient(false); // 実在するかチェック falseでエラー出す true(デフォ)は勝手に補正される
+            try {
+                Date date = sdf.parse(dateS);
+                return true;
+            } catch (ParseException e) {
+                return false;
+            }
         }
     }
-}
